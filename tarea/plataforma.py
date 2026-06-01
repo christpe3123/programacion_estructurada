ba6f2bb157eff85f43b4438e46de467bbf9d6c47
christpe3123 <bot39152@gmail.com>
2026-06-01 10:09:38 -0500

    tarea1

--- tarea/plataforma.py
+++ tarea/plataforma.py
@@ -1,1 +1,9 @@
 import login    
+def mostrar_plataforma(usuario):
+    if login.autenticar(usuario.usuario, usuario.contraseña):
+        token = login.tokenmisar(usuario.usuario)
+        alerta = login.alertar(usuario.usuario)
+        redireccion = login.redirigir(usuario.usuario)
+        return f"Bienvenido a la plataforma, {usuario.usuario}!\n{alerta}\nToken: {token}\n{redireccion}"
+    else:
+        return "Error: No se pudo acceder a la plataforma debido a credenciales incorrectas"
